172fb0e0189c05b872c290f3d411d91bd1fb9739
joselesquinas <tecnicjoncs@gmail.com>
2022-01-05 16:48:58 +0100

    ejercicio 4

--- src/films.js
+++ src/films.js
@@ -1,16 +1,16 @@
 // Exercise 1: Get the array of all directors.
 // director
 function getAllDirectors(array) {
-  let result = array.map(item => `${item.director}`);
-  console.log("EXERCICE 1 ->", result);
-  return result;
+   let result = array.map(item => `${item.director}`);
+   console.log("EXERCICE 1 ->", result);
+   return result;
 }
 
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
-   const result = array.filter( films => films.director === director);
-  return result;
+   const result = array.filter(films => films.director === director);
+   return result;
 }
 
 
@@ -18,26 +18,28 @@ function getMoviesFromDirector(array, director) {
 function moviesAverageOfDirector(array, director) {
    let movieDirector = x => x['director'] === director;
    let directors = array.filter(movieDirector).map(x => x.score);
-   let average = (directors.reduce((acc,val) => {
-     return acc + val;
-   } ,0) / (directors.length));
+   let average = (directors.reduce((acc, val) => {
+      return acc + val;
+   }, 0) / (directors.length));
    return average;
 }
 
 
 // TODO Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+   let result = [];
+   result = (array.map(item => `${item.title}`).sort()).slice(0, 20);
+   return result;
 }
 
 
 
 
-// Exercise 5: Order by year, ascending
+// TODO Exercise 5: Order by year, ascending
 function orderByYear() {
 
 
-   
+
 }
 
 // Exercise 6: Calculate the average of the movies in a category
@@ -52,7 +54,7 @@ function hoursToMinutes() {
 
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear() {
-  
+
 }
 
 
@@ -60,14 +62,14 @@ function bestFilmOfYear() {
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
-  module.exports = {
-    getAllDirectors,
-    getMoviesFromDirector,
-    moviesAverageOfDirector,
-    orderAlphabetically,
-    orderByYear,
-    moviesAverageByCategory,
-    hoursToMinutes,
-    bestFilmOfYear,
-  };
+   module.exports = {
+      getAllDirectors,
+      getMoviesFromDirector,
+      moviesAverageOfDirector,
+      orderAlphabetically,
+      orderByYear,
+      moviesAverageByCategory,
+      hoursToMinutes,
+      bestFilmOfYear,
+   };
 }
